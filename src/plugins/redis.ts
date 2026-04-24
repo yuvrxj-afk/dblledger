35e22530d72d8699348a1998a8d4aace40f4438d
yuvrxj-afk <yuvrajsisodiya7302@gmail.com>
2026-04-24 18:58:06 +0530

    feat(portfolio): transactions migration, graceful redis plugin

--- src/plugins/redis.ts
+++ src/plugins/redis.ts
@@ -1,4 +1,16 @@
 import Redis from "ioredis";
 import { REDIS_URL } from "../config/env";
 
-export const redis = new Redis(REDIS_URL)
+export const redis = new Redis(REDIS_URL, { lazyConnect: true, maxRetriesPerRequest: null })
+
+redis.on("error", (err) => {
+    console.error("[Redis] connection error:", err.message)
+})
+
+redis.on("connect", () => {
+    console.log("[Redis] connected");
+});
+
+redis.on("close", () => {
+    console.warn("[Redis] connection closed");
+});
